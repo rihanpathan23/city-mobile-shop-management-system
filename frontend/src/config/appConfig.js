6cb1cdac82ef1401db6ec3c816d9370c1e1a6463
Rihan Pathan <rp3948430@gmail.com>
2026-07-22 22:17:13 +0530

    feat: add centralized application configuration

---
+++ frontend/src/config/appConfig.js
@@ -0,0 +1,11 @@
+export const appConfig = {
+  appName: "City Mobile Shop Management System",
+  appVersion: "1.0.0",
+  companyName: "City Mobile Shop",
+  supportWhatsApp: "",
+  supportEmail: "",
+  currency: "INR",
+  dateFormat: "DD/MM/YYYY",
+};
+
+export default appConfig;
