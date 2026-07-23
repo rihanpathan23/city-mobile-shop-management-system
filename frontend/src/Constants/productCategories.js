83998aa509239e38de7ecfa8822d439539caf425
Rihan Pathan <rp3948430@gmail.com>
2026-07-23 08:09:04 +0530

    feat: add centralized product category constants

---
+++ frontend/src/Constants/productCategories.js
@@ -0,0 +1,19 @@
+export const PRODUCT_CATEGORIES = [
+  "Second Hand Phone",
+  "Mobile Cover",
+  "Screen Guard",
+  "Charger",
+  "Cable",
+  "Adapter",
+  "Headphones",
+  "Neckband",
+  "Bluetooth Speaker",
+  "Power Bank",
+  "Car Charger",
+  "Mobile Stand",
+  "Memory Card",
+  "SIM Accessories",
+  "Other Accessories",
+];
+
+export default PRODUCT_CATEGORIES;
